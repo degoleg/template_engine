c70db6d43dc3844fa111106189cb23ca4621579b
Олега <degoleg@yandex.ru>
2022-12-21 23:00:30 +0400

    add email

--- main.py
+++ main.py
@@ -6,6 +6,7 @@ app = Flask(__name__)
 def hello():
     name = "Ivan"
     phone = "+7 123 456 78 90"
+
     return render_template('hello.html', name=name, phone=phone)
 
 if __name__ == '__main__':
