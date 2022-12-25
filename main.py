d7aa0800cdf8bf327fb59a71c7dc520fd9ad2061
Олега <degoleg@yandex.ru>
2022-12-25 21:35:46 +0400

    cicle

--- main.py
+++ main.py
@@ -4,13 +4,8 @@ app = Flask(__name__)
 
 @app.route('/')
 def hello():
-    user_data = {
-        "name": "Ivan",
-        "phone": "+7 123 456 78 90",
-        "email": "ivan_dev@gmail.com",
-        "telegram": "ivan_dev",
-    }
-    return render_template('hello.html', user=user_data)
+        items = ["Python", "Java", "Kotlin", "Go"]
+        return render_template('hello.html', items=items)
 
 
 
