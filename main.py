ab8ffeebcb785f16a0ba82fa927bf55750dbe098
Олега <degoleg@yandex.ru>
2022-12-25 20:51:31 +0400

    add dict

--- main.py
+++ main.py
@@ -4,10 +4,16 @@ app = Flask(__name__)
 
 @app.route('/')
 def hello():
-    name = "Ivan"
-    phone = "+7 123 456 78 90"
+    user_data = {
+        "name": "Ivan",
+        "phone": "+7 123 456 78 90",
+        "email": "ivan_dev@gmail.com",
+        "telegram": "ivan_dev",
+    }
+    return render_template('hello.html', user=user_data)
 
-    return render_template('hello.html', name=name, phone=phone)
+
+
 
 if __name__ == '__main__':
     app.run()
